b87def2558738fb2a6b00041b84137a6e682abbd
Alex Lyzhoft <a72383@andersencorp.com>
2021-08-27 15:45:41 -0500

    Changes to Admin Page

--- front/src/Views/Admin.tsx
+++ front/src/Views/Admin.tsx
@@ -63,7 +63,7 @@ export default function Admin({ path }: Props) {
 	console.log({ data: data.carriers, selected });
 
 	return (
-		<div style={screenHeight} className="min-w-full my-12 rounded-md overflow-hidden flex flex-col">
+		<div style={screenHeight} className="min-w-full rounded-md overflow-hidden flex flex-col">
 			{addOpen && selected === 'Carriers' ? (
 				<AddCarriersModal open={addOpen} close={() => setAddOpen(false)} />
 			) : (
@@ -87,7 +87,7 @@ export default function Admin({ path }: Props) {
 				<button onClick={() => setAddOpen(true)} className=" shadow-lg mr-5 border-2 border-black">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						className="h-10 w-10"
+						className="h-5 w-5"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
